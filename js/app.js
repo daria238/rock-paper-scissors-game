e3a48254f6c9523eeb5518439c540a32ea8ee7fd
daria238 <dar2.4soi7po2@gmail.com>
2025-07-19 20:01:04 +0400

    created computerChoice function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 let choice;
 let color;
+let computerChoice
+let computerColor
 
 function showRulesModal() {
     document.getElementById("rules-modal").classList.remove("hidden");
@@ -18,6 +20,7 @@ function trackUserChoice(element) {
     document.getElementById("step-two").style.display="block";
 
     setUserChoiceDiv();
+    setComputerChoiceDiv();
 }
 
 function setUserChoiceDiv() {
@@ -47,3 +50,42 @@ function setUserChoiceDiv() {
           </div>
         </div>
 */
+
+function setComputerChoiceDiv() {
+    computerChoice = makeComputerIconChoice();
+    let computerChoiceDiv = document.getElementById("computer-choice-div");
+
+    let circleButtonDiv = document.createElement('div');
+    circleButtonDiv.classList.add("circle-button");
+    circleButtonDiv.classList.add(computerColor);
+
+    let innerCircleDiv = document.createElement("div");
+    innerCircleDiv.classList.add("inner-circle");
+
+    let imageIcon = document.createElement("img");
+    imageIcon.src = "images/icon-" + computerChoice + ".svg";
+    
+    innerCircleDiv.appendChild(imageIcon);
+    circleButtonDiv.appendChild(innerCircleDiv);
+    computerChoiceDiv.appendChild(circleButtonDiv);
+}
+
+function getRandomNumberInRange(min, max) {
+    let randomNumber = Math.random();
+    return Math.floor(randomNumber * max) + min;
+}
+
+function makeComputerIconChoice() {
+    let randomNumber = getRandomNumberInRange(1, 3);
+    if(randomNumber == 1) {
+      computerColor = "red";
+      return "rock";
+    } else if(randomNumber == 2) {
+      computerColor = "blue";
+      return "paper";
+    } else if(randomNumber == 3) {
+      computerColor = "yellow";
+      return "scissors";
+    }
+}
+
